bbcaad4bdc37379400114558999b3c9386831dee
priyanka-naveen-github <47553502+priyanka-naveen-github@users.noreply.github.com>
2020-08-20 10:40:42 +0530

    Add files via upload

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -7,10 +7,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"/src/test/resources/APIFeatureTests"},
+        features = "./src/test/resources/APIFeatureTests",
         glue = "stepDefinitions",
+        plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-reports/CucumberTestReport.json",
+                "pretty:target/cucumber-pretty.txt",
+                "usage:target/cucumber-usage.json"},
         monochrome = true
         //strict = false
 )
+
 public class TestRunner {
+
 }
--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,12 +1,7 @@
 package stepDefinitions;
 
 
-import java.util.List;
-import java.util.Map;
-
-//import io.cucumber.core.internal.gherkin.deps.com.google.gson.JsonObject;
-import org.junit.Assert;
-
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -14,45 +9,130 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
 
 
 public class Steps {
-    private static final String USER_ID = "5cf4bc25-6c52-450c-ac9b-b9218812fbc3";
-    // 44803acb-1f41-4083-8201-8d8ad1089ca6 - Shailesh
-    // 5d8b48f8-7112-4197-8281-e593319c32da - Neha
-    private static final String USERNAME = "shailesh_naveen";
-    // shailesh
-    private static final String PASSWORD = "Shail#123";
-    // Shail#123
+    //private static final String USER_ID = "5cf4bc25-6c52-450c-ac9b-b9218812fbc3";
     private static final String BASE_URL = "https://bookstore.toolsqa.com";
 
-
+    private static String USER_ID;
+    private static String USERNAME;
+    private static String PASSWORD;
     private static String token;
     private static Response response;
     private static String jsonString;
     private static String bookId;
+    private Object objList;
 
 
-    @Given("I am an authorized user")
-    public void iAmAnAuthorizedUser() {
+    @Given("I am an authorized user for {string} and {string}")
+    public void iAmAnAuthorizedUserForAnd(String User_Nm, String User_Pwd) throws PendingException {
+
+        //Given("^I am an authorized user for \"([^\"]*)\" and \"([^\"]*)\"$", (String User_Nm, String User_Pwd) -> {
+        //public void iAmAnAuthorizedUser(String User_Nm, String User_Pwd) throws PendingException{
+
+        final SimpleDateFormat simpleDtFmt = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+        //method 1
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        //System.out.println(timestamp);
+
+        //method 2 - via Date
+        //Date date = new Date();
+        //System.out.println(new Timestamp(date.getTime()));
+
+        //return number of milliseconds since January 1, 1970, 00:00:00 GMT
+        //System.out.println(timestamp.getTime());
+
+        //format timestamp
+        //System.out.println(simpleDtFmt.format(timestamp));
 
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
 
-        request.header("Content-Type", "application/json");
-        response = request.body("{ \"userName\":\"" + USERNAME + "\", \"password\":\"" + PASSWORD + "\"}").post("/Account/v1/GenerateToken");
+        USERNAME = User_Nm + timestamp.getTime();
+        System.out.println("Username: " + USERNAME);
+        PASSWORD = User_Pwd;
+        System.out.println("Password: " + PASSWORD);
 
-        System.out.println("\n\n\nI am an authorized use| Response Status: " + response.getStatusLine());
-
-        String jsonString = response.asString();
-        System.out.println(jsonString);
         /*
-        //token = JsonPath.from(jsonString).get("token");
-        //System.out.println(token);
-         */
-        //token = JsonPath.from(jsonString).getString("[0].token");
-        //token = JsonPath.from(jsonString).getString("token");
-        System.out.println("Authorized User, with Token Key: " + token);
+            The POST API to create a new user with the help of login credentials provided
+            i.e. 'username' and 'password'
+        */
+        try {
+            request.header("Content-Type", "application/json");
+            response = request.body("{ \"userName\": \"" + USERNAME + "\", \"password\": \"" + PASSWORD + "\"}").post("/Account/v1/User");
+            System.out.println("\nI am newly registered user| Response Status: " + response.getStatusLine());
+            jsonString = response.getBody().asString();
+            System.out.println("Response:\n" + jsonString);
+
+
+            String[] sResponseArray;
+            sResponseArray = jsonString.split("\"");
+            USER_ID = sResponseArray[3];
+            System.out.println("I am an authorized user| User-ID: " + USER_ID);
+
+        /*
+            The POST API to generate a token for newly registered user on BookStore APP with the help of login credentials provided
+            i.e. 'username' and 'password' to make it authorised
+        */
+            request.header("Content-Type", "application/json");
+            response = request.body("{ \"userName\":\"" + USERNAME + "\", \"password\":\"" + PASSWORD + "\"}").post("/Account/v1/GenerateToken");
+
+            // Reader header of a give name:- Header named Content-Type
+            String contentType = response.header("Content-Type");
+            System.out.println("Content-Type value: " + contentType);
+            Assert.assertEquals(contentType /* actual value */, "application/json; charset=utf-8" /* expected value */);
+
+            // Reader header of a give name:- Header named Server
+            String serverType = response.header("Server");
+            System.out.println("Server value: " + serverType);
+            //Assert.assertEquals(serverType /* actual value */, "nginx/1.12.1" /* expected value */);
+
+            // Reader header of a give name:- Header named Content-Encoding
+            String acceptLanguage = response.header("Content-Encoding");
+            System.out.println("Content-Encoding: " + acceptLanguage);
+            //Assert.assertEquals(contentEncoding /* actual value */, "gzip" /* expected value */);
+
+            int iStatusCode = response.getStatusCode();
+            String sStatusLine = response.getStatusLine();
+            System.out.println("Status Code : " + iStatusCode);
+            System.out.println("Status Line : " + sStatusLine);
+            //Assert.assertEquals(sStatusLine /*actual value*/, "HTTP/1.1 200 OK" /*expected value*/, "Valid status code returned !!");
+
+
+            String jsonString = response.getBody().asString();
+            System.out.println("Response:\n" + response.getBody().asString());
+
+
+            /*
+            JsonPath jsonPath = new JsonPath(jsonString);
+            System.out.println("Code Breaks 1!!");
+            // token = JsonPath.from(jsonString).get("token");
+            JsonPath jsonPathEvaluator = response.jsonPath();
+            System.out.println("Code Breaks 2!!");
+
+            // Exception: java.lang.NoClassDefFoundError: org/apache/groovy/io/StringBuilderWriter
+            //String sToken = (String) jsonPathEvaluator.get("$");
+            String sToken = response.jsonPath().getString("$");
+            System.out.println("Code Breaks 3!!");
+            */
+
+            // String[] sResponseArray;
+            sResponseArray = jsonString.split("\":\"");
+            token = sResponseArray[1];
+            System.out.println("I am an authorized user| Generated Token: " + token);
+
+        } catch (PendingException pe) {
+            System.out.println("Pending Exception| Exception raised: " + pe.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception| Exception raised: " + e.getMessage());
+        }
 
     }
 
@@ -66,13 +146,18 @@ public class Steps {
         System.out.println("\n\n\nA list of books are available| Response Status: " + response.getStatusLine());
 
         jsonString = response.asString();
-        System.out.println(jsonString);
-        /*
+        System.out.println("Response:\n" + jsonString);
+        /* Code Not Working
         List<Map<String, String>> books = JsonPath.from(jsonString).get("books");
-        //Assert.assertTrue(books.size() > 0);
+        Assert.assertTrue(books.size() > 0);
 
         bookId = books.get(0).get("isbn");
         */
+        String[] sResponseArray = jsonString.split("\"");
+        bookId = sResponseArray[5];
+        System.out.println("A list of books are available| ISBN of 1st Record: " + bookId);
+
+
     }
 
     @When("I add a book to my reading list")
@@ -80,34 +165,34 @@ public class Steps {
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
 
-        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InNoYWlsZXNoX25hdmVlbiIsInBhc3N3b3JkIjoiU2hhaWwjMTIzIiwiaWF0IjoxNTk2NTY1MDc1fQ.z-6DIZNaiXu-quSO5Gy7CzHYzK3nw6ABOLU4yFP5CVw";
-        request.header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
+        request.header("Content-Type", "application/json");
+        response = request.body("{ \"userName\":\"" + USERNAME + "\", \"password\":\"" + PASSWORD + "\"}").post("/Account/v1/GenerateToken");
+        jsonString = response.asString();
+        System.out.println("Response:\n" + jsonString);
 
-        response = request.body("{ \"userId\": \"" + USER_ID + "\", " +
-                "\"collectionOfIsbns\": [ { \"isbn\": \"" + bookId + "\" } ]}")
-                .post("/BookStore/v1/Books");
+        System.out.println("User_ID: " + USER_ID);
+        request.header("Authorization", "Bearer " + token).header("Content-Type", "application/json");
+        response = request.body("{ \"userId\": \"" + USER_ID + "\", " + "\"collectionOfIsbns\": [ { \"isbn\": \"" + bookId + "\" } ]}").post("/BookStore/v1/Books");
+        jsonString = response.asString();
+        System.out.println("Response:\n" + jsonString);
 
         System.out.println("\n\n\nI add a book to my reading list| Response Status: " + response.getStatusLine());
-
-        jsonString = response.asString();
-        System.out.println(jsonString);
     }
 
 
-    @Then("The book is added")
+    @Then("The book is added to my reading list")
     public void bookIsAdded() {
 
         //Assert.assertEquals(201, response.getStatusCode());
-        String bookId = "9781449331818";
+        //String bookId = "9781449331818";
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
-        response = request.get("/BookStore/v1/Book?ISBN=9781449325862");
+        response = request.get("/BookStore/v1/Book?ISBN=" + bookId);
 
         System.out.println("\n\n\nThe book is added| Response Status: " + response.getStatusLine());
 
         jsonString = response.asString();
-        System.out.println(jsonString);
+        System.out.println("Response:\n" + jsonString);
 
         Assert.assertEquals(200, response.getStatusCode());
         // throw new io.cucumber.java.PendingException();
@@ -124,21 +209,28 @@ public class Steps {
 
     @When("I remove a book from my reading list")
     public void removeBookFromList() {
+
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
 
-        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InNoYWlsZXNoX25hdmVlbiIsInBhc3N3b3JkIjoiU2hhaWwjMTIzIiwiaWF0IjoxNTk2NTY1MDc1fQ.z-6DIZNaiXu-quSO5Gy7CzHYzK3nw6ABOLU4yFP5CVw";
-        request.header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
-
-        System.out.println("\n\n\nUser ID: " + USER_ID);
-        response = request.body("{ \"isbn\": \"" + bookId + "\", \"userId\": \"" + USER_ID + "\"}")
-                .delete("/BookStore/v1/Book");
-
-        System.out.println("I remove a book from my reading list| Response Status: " + response.getStatusLine());
-        Assert.assertEquals(204, response.getStatusCode());
+        /*
+        request.header("Content-Type", "application/json");
+        response = request.body("{ \"userName\": \"" + USERNAME + "\", \"password\": \"" + PASSWORD + "\"}").post("/Account/v1/User");
         jsonString = response.asString();
-        System.out.println(jsonString);
+        System.out.println("Response:\n" + jsonString);
+        */
+
+        System.out.println("User ID: " + USER_ID);
+        System.out.println("Book ID: " + bookId);
+        request.header("Authorization", "Bearer " + token).header("Content-Type", "application/json");
+        response = request.body("{ \"isbn\": \"" + bookId + "\", \"userId\": \"" + USER_ID + "\"}").delete("/BookStore/v1/Book");
+        jsonString = response.asString();
+        System.out.println("Response:\n" + jsonString);
+
+
+        /*
+        curl -X POST "https://bookstore.toolsqa.com/Account/v1/User" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"userName\": \"shailesh_naveen\", \"password\": \"Shall#123\"}"
+         */
     }
 
 
@@ -160,6 +252,7 @@ public class Steps {
         System.out.println("The book is removed| Response Status: " + response.getStatusLine());
         Assert.assertEquals(0, booksOfUser.size());
     }
+
     /*
     @Then("the book is removed")
     public void the_book_is_removed() {
@@ -167,5 +260,5 @@ public class Steps {
         throw new io.cucumber.java.PendingException();
     }
     */
-}
 
+}
